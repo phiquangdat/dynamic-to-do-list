635ebcfd452b4a0d4f6e75e970057dfbdeee2acd
phiquangdat <datphi020603@gmail.com>
2025-02-27 15:43:54 +0200

    handleEvent so that can click the text field to mark as completed

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ $(document).ready(function () {
   $("#submit-task").on("click", function () {
     let task = $("#new-task").val();
     let component = `
-    <label>
+    <div class="task-container">
       <input type="checkbox" class="task-checkbox">
-      <span></span>
-      ${task}
-    </label>
+      <span class="task-text">${task}</span>
+    </div">
     `;
     $("#task-list").append(component);
     $("#new-task").val("");
   });
 
   $(document).on("change", ".task-checkbox", function () {
-    $(this).closest("label").toggleClass("completed");
+    $(this).closest(".task-container").toggleClass("completed");
+  });
+
+  $(document).on("click", ".task-text", function () {
+    $(this).closest(".task-container").toggleClass("completed");
+
+    // Toggle the checkbox's checked property
+    let checkbox = $(this).siblings(".task-checkbox");
+    checkbox.prop("checked", !checkbox.prop("checked"));
   });
 });
