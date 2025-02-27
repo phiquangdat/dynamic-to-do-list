0161074d95d80c69effce569a14865e453ef0a51
phiquangdat <datphi020603@gmail.com>
2025-02-27 14:51:13 +0200

    add the checkbox next to added task

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
   $("#add-task").on("click", function () {
     $(".add-task-input").removeAttr("hidden");
   });
+
   $("#submit-task").on("click", function () {
     let task = $("#new-task").val();
-    let component = `<li>${task}</li>`;
+    let component = `
+    <label>
+      <input type="checkbox">
+      <span></span>
+      ${task}
+    </label>
+    `;
     $("#task-list").append(component);
     $("#new-task").val("");
   });
