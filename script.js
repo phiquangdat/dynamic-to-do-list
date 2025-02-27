5db4c8e630b4646c14aaa5a54d371e6e8ba0790d
phiquangdat <datphi020603@gmail.com>
2025-02-27 15:01:45 +0200

    strikethrough completed tasks

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ $(document).ready(function () {
     let task = $("#new-task").val();
     let component = `
     <label>
-      <input type="checkbox">
+      <input type="checkbox" class="task-checkbox">
       <span></span>
       ${task}
     </label>
     `;
     $("#task-list").append(component);
     $("#new-task").val("");
+  });
+
+  $(document).on("change", ".task-checkbox", function () {
+    $(this).closest("label").toggleClass("completed");
   });
 });
